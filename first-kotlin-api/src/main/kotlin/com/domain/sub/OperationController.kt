381905d8604c5c32e650cb8c1a424b6245b96892
Rafael <noprafa@gmail.com>
2023-08-03 21:27:30 -0300

    refactor(Exception -> UnsupportedMathOperationException): Changing exception handling to more specific message

--- first-kotlin-api/src/main/kotlin/com/domain/sub/OperationController.kt
+++ first-kotlin-api/src/main/kotlin/com/domain/sub/OperationController.kt
@@ -1,17 +1,18 @@
 package com.domain.sub
 
+import com.domain.sub.exceptions.UnsupportedMathOperationException
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class OperationController {
-    @RequestMapping(value=["/sum/{numberOne}/{numberTwo}"])
+    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
     fun sum(
             @PathVariable(value = "numberOne") numberOne: String?,
             @PathVariable(value = "numberTwo") numberTwo: String?
     ): Operation {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw UnsupportedMathOperationException("Plase set a numeric value")
         return Operation(convertDouble(numberOne) + convertDouble(numberTwo))
     }
 
