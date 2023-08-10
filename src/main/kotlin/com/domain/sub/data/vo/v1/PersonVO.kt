dc37c4e9b6d790a3e2aa6264a1ebc73d290f6781
Rafael <noprafa@gmail.com>
2023-08-10 00:41:07 -0300

    add(json properties do VO object returned)

--- src/main/kotlin/com/domain/sub/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/com/domain/sub/data/vo/v1/PersonVO.kt
@@ -1,19 +1,34 @@
 package com.domain.sub.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import java.util.*
 
 // javax persistence -> jakarta persistence
 
+@JsonPropertyOrder(
+    "id",
+    "address",
+    "firstName",
+    "lastName",
+    "gender"
+)
 data class PersonVO(
 
     var id: UUID? = null,
-
+    // To change name returned by the VO class
+    @field:JsonProperty("first_name")
     var firstName: String = "",
 
+    @field:JsonProperty("last_name")
     var lastName: String = "",
+
 
     var address: String = "",
 
+    // Not show field
+    @field:JsonIgnore
     var gender: String = "",
 
     )
